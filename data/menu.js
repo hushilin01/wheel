be99999fdf0d34d9232167cf297b0d3710aa0cfe
3471433975@qq.com <3471433975@qq.com>
2020-02-05 13:18:05 +0800

    吃饱了没事干改点东西

--- data/menu.js
+++ data/menu.js
@@ -1,9 +1,23 @@
 var menuList = [{
-	name: 'js',
+	name: 'javascript',
+	dir: 'javascript',
 	list: [{
 		name: '一个promise的轮子',
-		path: ''
+		introduction1: '',
+		introduction2: '',
+		path: '',
+		time: '2019-01-01'
 	}],
-	length: 1
+
+}, {
+	name: '记章',
+	dir: 'history',
+	list: [{
+		name: '一个promise的轮子',
+		introduction1: '',
+		introduction2: '',
+		path: '',
+		time: '2019-01-01'
+	}],
 
 }]
